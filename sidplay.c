aaf537f7ecd00786cc5df08e06f355252e52d9c0
kklis <krzysztof.klis@gmail.com>
2022-04-03 18:43:02 +0200

    More c, less asm

--- sidplay.c
+++ sidplay.c
@@ -9,24 +9,27 @@ int main() {
     unsigned char fnamelen = strlen(fname);
 
     // Load SID file into memory
-    asm ("JSR $FFBD\n\t"  // SETNAM (A = fname lenght, XY = fname address)
-         "LDA #$01\n\t"   // logical file number
-         "LDX #$08\n\t"   // device number
-         "LDY #$01\n\t"   // load to address found in file header
-         "JSR $FFBA\n\t"  // SETLFS
-         "LDA #$00\n\t"   // load to memory
-         "JSR $FFD5\n\t"  // LOAD
-         :: "a"(fnamelen), "x"(fnamelo), "y"(fnamehi));
+    asm("JSR $FFBD\n\t"  // SETNAM (A = fname lenght, XY = fname address)
+        "LDA #$01\n\t"   // logical file number
+        "LDX #$08\n\t"   // device number
+        "LDY #$01\n\t"   // load to address found in file header
+        "JSR $FFBA\n\t"  // SETLFS
+        "LDA #$00\n\t"   // load to memory
+        "JSR $FFD5\n\t"  // LOAD
+        :: "a"(fnamelen), "x"(fnamelo), "y"(fnamehi));
+
     // Init SID player routine
-    asm ("LDA #$00\n\t"
-         "TAX\n\t"
-         "TAY\n\t"
-         "JSR $1000");
+    asm("LDA #$00\n\t"
+        "TAX\n\t"
+        "TAY\n\t"
+        "JSR $1000");
+
     // Call SID refresh routine
     while (1) {
         if (rasterline == 255) {
             asm ("JSR $1003");
         }
     }
+
     return 0;
 }
--- sidplayint.c
+++ sidplayint.c
@@ -1,9 +1,17 @@
 #include <string.h>
 
+#define intctrl (*((volatile unsigned char*)0xDC0D))
+#define screenctrl (*((volatile unsigned char*)0xD011))
+#define rasterline (*((volatile unsigned char*)0xD012))
+#define rasterintctrl (*((volatile unsigned char*)0xD01A))
+#define rasterintlo (*((volatile unsigned char*)0x314))
+#define rasterinthi (*((volatile unsigned char*)0x315))
+
+// SID refresh function
 void play() {
-    asm ("ASL $D019\n\t"  // acknowledge interrupt (reset bit 0)
-         "JSR $1003\n\t"  // call SID refresh routine
-         "JMP $EA31");    // jump to default interrupt handler routine
+    asm("ASL $D019\n\t"  // acknowledge interrupt (reset bit 0)
+        "JSR $1003\n\t"  // call SID refresh routine
+        "JMP $EA31");    // jump to default interrupt handler routine
 }
 
 int main() {
@@ -12,35 +20,32 @@ int main() {
     unsigned char fnamehi = (unsigned int)fname >> 8;
     unsigned char fnamelen = strlen(fname);
     void (* fun)(void) = &play;
-    unsigned char funlo = (unsigned int)fun & 0xFFFF;
-    unsigned char funhi = (unsigned int)fun >> 8;
 
     // Load SID file into memory
-    asm ("JSR $FFBD\n\t"  // SETNAM (A = fname lenght, XY = fname address)
-         "LDA #$01\n\t"   // logical file number
-         "LDX #$08\n\t"   // device number
-         "LDY #$01\n\t"   // load to address found in file header
-         "JSR $FFBA\n\t"  // SETLFS
-         "LDA #$00\n\t"   // load to memory
-         "JSR $FFD5\n\t"  // LOAD
-         :: "a"(fnamelen), "x"(fnamelo), "y"(fnamehi));
+    asm("JSR $FFBD\n\t"  // SETNAM (A = fname lenght, XY = fname address)
+        "LDA #$01\n\t"   // logical file number
+        "LDX #$08\n\t"   // device number
+        "LDY #$01\n\t"   // load to address found in file header
+        "JSR $FFBA\n\t"  // SETLFS
+        "LDA #$00\n\t"   // load to memory
+        "JSR $FFD5\n\t"  // LOAD
+        :: "a"(fnamelen), "x"(fnamelo), "y"(fnamehi));
+
     // Init SID player routine
-    asm ("LDA #$00\n\t"
-         "TAX\n\t"
-         "TAY\n\t"
-         "JSR $1000");
+    asm("LDA #$00\n\t"
+        "TAX\n\t"
+        "TAY\n\t"
+        "JSR $1000");
+
     // Set up raster interrupt
-    asm ("SEI\n\t"        // switch off interrupts
-         "LDA #$01\n\t"   // enable raster interrupts
-         "STA $D01A\n\t"
-         "STX $0314\n\t"  // low byte of raster interrupt routine
-         "STY $0315\n\t"  // high byte of raster interrupt routine
-         "LDA #$7F\n\t"   // disable CIA interrupts
-         "STA $DC0D\n\t"
-         "AND $D011\n\t"  // clear bit 8 of raster comparator
-         "STA $D011\n\t"
-         "STA $D012\n\t"  // trigger interrupt at raster line 127
-         "CLI"            // switch on interrupts
-         :: "x"(funlo), "y"(funhi));
+    asm("SEI");         // switch off interrupts
+    intctrl = 0x7F;     // disable CIA interrupts
+    rasterintctrl = 1;  // enable raster interrupts
+    rasterintlo = (unsigned int)fun & 0xFFFF; // low byte of raster interrupt routine
+    rasterinthi = (unsigned int)fun >> 8;     // high byte of raster interrupt routine
+    rasterline = 127;   // trigger interrupt at raster line 127
+    screenctrl = screenctrl & 0x7F;  // ignore rasterlines > 255
+    asm("CLI");         // switch on interrupts
+
     return 0;
 }
